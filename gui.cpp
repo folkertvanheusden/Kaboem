c5f950956590d5d26def87abf4d1fed42f172148
Folkert van Heusden <mail@vanheusden.com>
2025-06-28 13:06:38 +0200

    show error when kaboem-file cannot be loaded

--- gui.cpp
+++ gui.cpp
@@ -826,24 +826,27 @@ int main(int argc, char *argv[])
 					if (fs_data.file.empty() == false) {
 						std::lock_guard<std::shared_mutex> lck(sound_pars.sounds_lock);
 						std::unique_lock<std::shared_mutex> pat_lck(pat_clickables_lock);
-						read_file(fs_data.file, &pat_clickables, &samples, &file_parameters);
+						if (read_file(fs_data.file, &pat_clickables, &samples, &file_parameters)) {
+							sound_pars.global_volume    = vol / 100.;
+							sound_pars.sound_saturation = 1. - sound_saturation / 1000.;
+							swing_amount_parameter      = swing_amount;
+							sleep_ms                    = 60 * 1000 / bpm;
+							menu_buttons_clickables[polyrythmic_idx].selected = polyrythmic;
+							for(size_t i=0; i<pattern_groups; i++) {
+								if (samples[i].name.empty() == false)
+									channel_clickables[i].text = get_filename(samples[i].name).substr(0, 5);
 
-						sound_pars.global_volume    = vol / 100.;
-						sound_pars.sound_saturation = 1. - sound_saturation / 1000.;
-						swing_amount_parameter      = swing_amount;
-						sleep_ms                    = 60 * 1000 / bpm;
-						menu_buttons_clickables[polyrythmic_idx].selected = polyrythmic;
-						for(size_t i=0; i<pattern_groups; i++) {
-							if (samples[i].name.empty() == false)
-								channel_clickables[i].text = get_filename(samples[i].name).substr(0, 5);
+							}
+							redraw = true;
+							menu_status = "file " + get_filename(fs_data.file) + " read";
+							regenerate_pattern_grid(display_mode->w, display_mode->h, &pat_clickables[pattern_group]);
 
+							pat_lck.unlock();
+							reset_all_patterns(&pat_clickables, &pat_clickables_lock, samples, false);
 						}
-						redraw = true;
-						menu_status = "file " + get_filename(fs_data.file) + " read";
-						regenerate_pattern_grid(display_mode->w, display_mode->h, &pat_clickables[pattern_group]);
-
-						pat_lck.unlock();
-						reset_all_patterns(&pat_clickables, &pat_clickables_lock, samples, false);
+						else {
+							menu_status = "cannot read file " + get_filename(fs_data.file);
+						}
 					}
 					fs_action = fs_none;
 				}
