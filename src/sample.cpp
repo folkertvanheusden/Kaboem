folkertvanheusden/Kaboem

commit 9052e04f3b54f256a50dee4594f240b6ff97aea8

code clean-up

src/sample.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "frequencies.h"
+#include "sample.h"
 
 
 float find_loudest_frequency(const std::vector<std::vector<float> > & samples, const unsigned sample_sample_rate)
@@ -24,14 +25,15 @@ float find_loudest_frequency(const std::vector<std::vector<float> > & samples, c
 	return loudest_frequency;
 }
 
-std::optional<std::tuple<std::vector<std::vector<float> > *, unsigned int, float> > load_sample(const std::string & filename)
+std::optional<sample_t> load_sample(const std::string & filename)
 {
         SF_INFO si = { 0 };
         SNDFILE *sh = sf_open(filename.c_str(), SFM_READ, &si);
 	if (!sh)
 		return { };
 
-	auto *samples = new std::vector<std::vector<float> >();
+	std::vector<std::vector<float> > samples;
+	samples.resize(si.channels);
 
 	constexpr int load_buffer_size = 4096;
 	float *buffer = new float[load_buffer_size * si.channels];
@@ -43,19 +45,18 @@ std::optional<std::tuple<std::vector<std::vector<float> > *, unsigned int, float
 
 		for(sf_count_t i=0; i<cur_n; i++) {
 			int offset = i * si.channels;
-
-			std::vector<float> row;
-			for(int j=offset; j<offset + si.channels; j++)
-				row.push_back(buffer[j]);
-			samples->push_back(row);
+			for(int ch=0; ch<si.channels; ch++)
+				samples[ch].push_back(buffer[offset + ch]);
 		}
 	}
 
 	sf_close(sh);
 	delete [] buffer;
 
-	float loudest_frequency = find_loudest_frequency(*samples, si.samplerate);
+	float loudest_frequency = find_loudest_frequency(samples, si.samplerate);
 	printf("loudest_frequency of \"%s\": %.1f Hz\n", filename.c_str(), loudest_frequency);
 
-	return { { samples, si.samplerate, loudest_frequency } };
+	sample_t out { std::move(samples), si.samplerate, loudest_frequency };
+
+	return out;
 }

src/sample.h
@@ -3,6 +3,11 @@
 #include <vector>
 
 
-// data, sample rate, (loudest-) frequency
-std::optional<std::tuple<std::vector<std::vector<float> > *, unsigned int, float> > load_sample(const std::string & filename);
+struct sample_t {
+	std::vector<std::vector<float> > samples;
+	int                              sample_rate;
+	float                            loudest_frequency;
+};
+
+std::optional<sample_t> load_sample(const std::string & filename);
 float find_loudest_frequency(const std::vector<std::vector<float> > & samples, const unsigned sample_sample_rate);

src/sound.cpp
@@ -35,9 +35,9 @@ bool sound_sample::begin()
 			printf("Cannot access sample \"%s\" in cache\n", file_name.c_str());
 			return false;
 		}
-		samples            = *std::get<0>(rc.value());
-		sample_sample_rate =  std::get<1>(rc.value());
-		base_frequency     =  ceil(std::get<2>(rc.value()));
+		samples            = std::move(rc.value().samples);
+		sample_sample_rate = rc.value().sample_rate;
+		base_frequency     = rc.value().loudest_frequency;
 	}
 	else {
 		base_frequency     = find_loudest_frequency(samples, sample_sample_rate);

src/sound.h
@@ -24,21 +24,6 @@
 
 double f_to_delta_t(const double frequency, const int sample_rate);
 
-class sound_control
-{
-public:
-	// name
-	enum { cm_continuous_controller } cm_mode;
-	uint8_t cm_index;  // midi index (for 0xb0: data-1)
-	int index;  // internal index
-	std::string name;
-	// how to transform a value
-	double divide_by;
-	double add;
-	// last transformed value
-	double current_setting;
-};
-
 class sound
 {
 protected:
@@ -55,8 +40,6 @@ protected:
 	// input channel, { output channel, volume }
 	std::vector<float> volumes;
 
-	std::vector<sound_control> controls;
-
 public:
 	sound(const int sample_rate, const double frequency) :
 		sample_rate(sample_rate),
@@ -64,19 +47,7 @@ public:
 	{
 	}
 
-	virtual std::vector<sound_control> get_controls()
-	{
-		return controls;
-	}
-
 	virtual size_t get_sample_count() const = 0;
-
-	virtual void set_control(const int nr, const int value)
-	{
-		printf("set control %d to %d: ", nr, value);
-		controls.at(nr).current_setting = value / controls.at(nr).divide_by + controls.at(nr).add;
-		printf("%f\n", controls.at(nr).current_setting);
-	}
 
 	void add_mapping(const int to, const float volume)
 	{

commit 5fa12e9368c13648ee57d98a1e87c4f4b7eef4c5

loading of samples was broken by 9052e04f3b54f256a50dee4594f240b6ff97aea8

src/sample.cpp
@@ -33,7 +33,6 @@ std::pair<std::optional<sample_t>, std::string> load_sample(const std::string & 
 		return { { }, sf_strerror(sh) };
 
 	std::vector<std::vector<float> > samples;
-	samples.resize(si.channels);
 
 	constexpr int load_buffer_size = 65536;
 	float *buffer = new float[load_buffer_size * si.channels];
@@ -45,8 +44,10 @@ std::pair<std::optional<sample_t>, std::string> load_sample(const std::string & 
 
 		for(sf_count_t i=0; i<cur_n; i++) {
 			int offset = i * si.channels;
-			for(int ch=0; ch<si.channels; ch++)
-				samples[ch].push_back(buffer[offset + ch]);
+			std::vector<float> row;
+			for(int j=offset; j<offset + si.channels; j++)
+				row.push_back(buffer[j]);
+			samples.push_back(row);
 		}
 	}
 
@@ -54,7 +55,7 @@ std::pair<std::optional<sample_t>, std::string> load_sample(const std::string & 
 	delete [] buffer;
 
 	float loudest_frequency = find_loudest_frequency(samples, si.samplerate);
-	printf("loudest_frequency of \"%s\": %.1f Hz\n", filename.c_str(), loudest_frequency);
+	printf("Loudest frequency of \"%s\": %.1f Hz\n", filename.c_str(), loudest_frequency);
 
 	sample_t out { std::move(samples), si.samplerate, loudest_frequency };
 